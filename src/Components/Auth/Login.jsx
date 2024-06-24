8c08bdf96a6cd35e9aa5c7b114868a292b63ac15
Saad Ziauddin <saadziauddin6@gmail.com>
2024-06-24 13:58:23 +0500

    commit

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
-// import '../../assets/css/style.css';
 import logo from '/assets/img/web/logo.png';
 import axios from 'axios';
 
@@ -10,6 +9,7 @@ function Login () {
 
     const [values, setValues] = useState({ email: '', password: '' });
     const navigate = useNavigate();
+    const API_URL = import.meta.env.VITE_API_URL;
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -19,7 +19,7 @@ function Login () {
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
-            const response = await axios.post('http://localhost:8090/', values, { withCredentials: true });
+            const response = await axios.post(`${API_URL}/`, values, { withCredentials: true });
             if (response.data.message === "Login successful!") {
                 setValues({ email: '', password: '' });
 
@@ -96,146 +96,6 @@ function Login () {
                 </div>
             </div>
         </div>
-
-    // <div className="bg-image flex items-center justify-center h-screen bg-cover bg-center bg-no-repeat">
-    //     <div className="flex items-center justify-center h-full w-full">
-    //         <div className="bg-white bg-opacity-10 backdrop-blur-md border border-white border-opacity-20 shadow-lg text-white rounded-2xl p-10 w-full max-w-md">
-
-    //             <div className="self-center mb-6">
-    //                 <img src={logo} alt="Pronet Logo" className="h-20 w-40 mx-auto" />
-    //             </div>
-
-    //             <div className="text-center text-white mb-6 text-md">
-    //                 Enter credentials to access your account
-    //             </div>
-
-    //             <form onSubmit={handleSubmit}>
-    //                 <div className="flex flex-col mb-6">
-    //                     <label htmlFor="email" className="mb-2 text-sm font-medium">E-Mail Address:</label>
-    //                     <div className="relative">
-    //                         <input 
-    //                             id="email" 
-    //                             type="email" 
-    //                             name="email" 
-    //                             className="text-sm placeholder-white bg-transparent pl-4 pr-12 rounded-full border border-white border-opacity-20 w-full py-2 focus:outline-none focus:ring-2 focus:ring-white focus:border-transparent" 
-    //                             autoFocus 
-    //                             placeholder="Enter your email" 
-    //                             required 
-    //                             onChange={handleChange} 
-    //                             value={values.email} 
-    //                         />
-    //                         <div className="absolute inset-y-0 right-0 flex items-center pr-3">
-    //                             <FontAwesomeIcon icon={faEnvelope} className="text-white" />
-    //                         </div>
-    //                     </div>
-    //                 </div>
-
-    //                 <div className="flex flex-col mb-8">
-    //                     <label htmlFor="password" className="mb-2 text-sm font-medium">Password:</label>
-    //                     <div className="relative">
-    //                         <input 
-    //                             id="password" 
-    //                             type="password" 
-    //                             name="password" 
-    //                             className="text-sm placeholder-white bg-transparent pl-4 pr-12 rounded-full border border-white border-opacity-20 w-full py-2 focus:outline-none focus:ring-2 focus:ring-white focus:border-transparent" 
-    //                             placeholder="Enter your password" 
-    //                             required 
-    //                             onChange={handleChange} 
-    //                             value={values.password} 
-    //                         />
-    //                         <div className="absolute inset-y-0 right-0 flex items-center pr-3">
-    //                             <FontAwesomeIcon icon={faLock} className="text-white" />
-    //                         </div>
-    //                     </div>
-    //                 </div>
-
-    //                 <div className="flex w-full">
-    //                     <button 
-    //                         type="submit" 
-    //                         className="flex items-center justify-center focus:outline-none text-white text-sm sm:text-base bg-gray-700 hover:bg-gray-800 rounded-full py-3 w-full transition duration-150 ease-in">
-    //                         <span className="mr-2 uppercase font-semibold">Login</span>
-    //                         <span>
-    //                             <svg className="h-6 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
-    //                                 <path d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
-    //                             </svg>
-    //                         </span>
-    //                     </button>
-    //                 </div>
-    //             </form>
-
-    //         </div>
-    //     </div>
-    // </div>
-
-    // <div className="bg-image flex items-center justify-center h-screen bg-cover bg-center bg-no-repeat">
-    // <div className="flex items-center justify-center h-full w-full">
-    //     <div className="flex flex-col bg-gray-300 bg-opacity-50 shadow-xl px-10 py-12 rounded-3xl w-full max-w-md">
-
-    //         <div className="self-center mb-6">
-    //             <img src={logo} alt="Pronet Logo" className="h-16 w-auto" />
-    //         </div>
-
-    //         <div className="text-center text-gray-800 mb-8 text-lg font-light">
-    //             Enter your credentials to access your account
-    //         </div>
-
-    //         <form onSubmit={handleSubmit}>
-    //             <div className="flex flex-col mb-6">
-    //                 <label htmlFor="email" className="mb-2 text-sm font-medium text-gray-700">E-Mail Address:</label>
-    //                 <div className="relative">
-    //                     <div className="absolute inset-y-0 left-0 flex items-center pl-3">
-    //                         <FontAwesomeIcon icon={faEnvelope} className="text-gray-400" />
-    //                     </div>
-    //                     {/* <input id="email" type="email" name="email" className="text-sm placeholder-gray-500 pl-10 pr-4 rounded-full border border-gray-300 w-full py-2 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent" autoFocus placeholder="Enter your email" required onChange={handleChange} value={values.email} /> */}
-    //                     <input 
-    //                         id="email" 
-    //                         type="email" 
-    //                         name="email" 
-    //                         className="text-sm placeholder-gray-300 text-white bg-transparent pl-12 pr-4 rounded-full border border-gray-600 w-full py-2 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent" 
-    //                         autoFocus 
-    //                         placeholder="Enter your email" 
-    //                         required 
-    //                         onChange={handleChange} 
-    //                         value={values.email} 
-    //                     />
-    //                 </div>
-    //             </div>
-
-    //             <div className="flex flex-col mb-8">
-    //                 <label htmlFor="password" className="mb-2 text-sm font-medium text-gray-700">Password:</label>
-    //                 <div className="relative">
-    //                     <div className="absolute inset-y-0 left-0 flex items-center pl-3">
-    //                         <FontAwesomeIcon icon={faLock} className="text-gray-400" />
-    //                     </div>
-    //                     {/* <input id="password" type="password" name="password" className="text-sm placeholder-gray-500 pl-10 pr-4 rounded-full border border-gray-600 w-full py-2 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent" placeholder="Enter your password" required onChange={handleChange} value={values.password} /> */}
-    //                     <input 
-    //                         id="password" 
-    //                         type="password" 
-    //                         name="password" 
-    //                         className="text-sm placeholder-gray-200 text-white bg-transparent pl-12 pr-4 rounded-full border border-gray-600 w-full py-2 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent" 
-    //                         placeholder="Enter your password" 
-    //                         required 
-    //                         onChange={handleChange} 
-    //                         value={values.password}
-    //                     />
-    //                 </div>
-    //             </div>
-
-    //             <div className="flex w-full">
-    //                 <button type="submit" className="flex items-center justify-center focus:outline-none text-white text-sm sm:text-base bg-gray-700 hover:bg-gray-800 rounded-full py-3 w-full transition duration-150 ease-in">
-    //                     <span className="mr-2 uppercase">Login</span>
-    //                     <span>
-    //                         <svg className="h-6 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
-    //                             <path d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
-    //                         </svg>
-    //                     </span>
-    //                 </button>
-    //             </div>
-    //         </form>
-
-    //     </div>
-    // </div>
-    // </div>
     );
 }
 
