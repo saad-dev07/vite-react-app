fa2e534b4fa14856ebfa7671cfdfa044cf235cc9
Saad Ziauddin <saadziauddin6@gmail.com>
2024-06-25 00:52:21 +0500

    commit

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -9,7 +9,8 @@ function Login () {
 
     const [values, setValues] = useState({ email: '', password: '' });
     const navigate = useNavigate();
-    // const API_URL = import.meta.env.VITE_API_URL || 'https://node-server-lyart.vercel.app/'; `${API_URL}/`
+    
+    const API_BASE_URL = import.meta.env.VITE_API_URL;
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -19,7 +20,7 @@ function Login () {
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
-            const response = await axios.post("http://localhost:8090/", values, { withCredentials: true });
+            const response = await axios.post(`${API_BASE_URL}/`, values, { withCredentials: true });
             if (response.data.message === "Login successful!") {
                 setValues({ email: '', password: '' });
                 const role = response.data.role;
