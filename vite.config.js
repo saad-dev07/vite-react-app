07a324cdba92438d856f3cf59dc6f8bbb5ed1b5b
Saad Ziauddin <saadziauddin6@gmail.com>
2024-07-01 15:46:43 +0500

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {
+      VITE_API_URL: process.env.VITE_API_URL,
+      NODE_ENV: process.env.NODE_ENV,
+      COOKIE_DOMAIN: process.env.COOKIE_DOMAIN
+    }
+  }
 })
+
