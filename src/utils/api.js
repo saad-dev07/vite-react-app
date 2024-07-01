a93a16397b9f7549e37fd6a9c21095202eb69d1e
Saad Ziauddin <saadziauddin6@gmail.com>
2024-07-01 11:37:25 +0500

    commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true;
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
