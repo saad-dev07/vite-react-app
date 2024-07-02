55e983efcd8ddce47454b5cfd6569a3788bf58ab
Saad Ziauddin <saadziauddin6@gmail.com>
2024-07-02 12:22:20 +0500

    commit

--- vite.config.js
+++ vite.config.js
@@ -11,13 +11,24 @@
 //     }
 //   }
 // })
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   define: {
+//     'process.env.NODE_ENV': '"production"',
+//   }
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env.NODE_ENV': '"production"',
+  build: {
+    outDir: 'dist'
   }
 });
 
