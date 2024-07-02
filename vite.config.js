e03fb63f7bd19c2d1b3899b2897ac94b0667bc58
Saad Ziauddin <saadziauddin6@gmail.com>
2024-07-02 13:48:57 +0500

    commit

--- vite.config.js
+++ vite.config.js
@@ -22,13 +22,33 @@
 //   }
 // });
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  // build: {
-  //   outDir: 'dist'
-  // }
-});
+// export default defineConfig({
+//   plugins: [react()],
+//   // build: {
+//   //   outDir: 'dist'
+//   // }
+// });
+
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    define: {
+      // Make the environment variables available in the Vite config
+      'process.env': {
+        VITE_API_URL: JSON.stringify(env.VITE_API_URL),
+        NODE_ENV: JSON.stringify(env.NODE_ENV),
+        COOKIE_DOMAIN: JSON.stringify(env.COOKIE_DOMAIN)
+      }
+    }
+  }
+})
 
