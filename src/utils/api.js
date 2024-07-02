57d0e3b83cd01cbc809eea69ec609629db773b09
Saad Ziauddin <saadziauddin6@gmail.com>
2024-07-02 14:33:31 +0500

    commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,17 +8,30 @@ const api = axios.create({
 });
 
 // Add a request interceptor to include the token
+// api.interceptors.request.use(
+//     config => {
+//         const token = localStorage.getItem('token'); // Replace with your token storage method
+//         if (token) {
+//         config.headers['Authorization'] = `Bearer ${token}`;
+//         }
+//         return config;
+//     },
+//     error => {
+//         return Promise.reject(error);
+//     }
+// );
+
 api.interceptors.request.use(
     config => {
-        const token = localStorage.getItem('token'); // Replace with your token storage method
-        if (token) {
-        config.headers['Authorization'] = `Bearer ${token}`;
-        }
-        return config;
+      const token = document.cookie.split('; ').find(row => row.startsWith('token='));
+      if (token) {
+        config.headers['Authorization'] = `Bearer ${token.split('=')[1]}`;
+      }
+      return config;
     },
     error => {
-        return Promise.reject(error);
+      return Promise.reject(error);
     }
-);
+  );
 
 export default api;
