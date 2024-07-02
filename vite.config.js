ab605629a62110d65336d9937d92a216f6b39b26
Saad Ziauddin <saadziauddin6@gmail.com>
2024-07-02 12:06:47 +0500

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   define: {
+//     'process.env': {
+//       VITE_API_URL: 'https://pronet-node-api.vercel.app',
+//       NODE_ENV: 'production',
+//       COOKIE_DOMAIN: 'pronet-application.vercel.app'
+//     }
+//   }
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {
-      VITE_API_URL: process.env.VITE_API_URL,
-      NODE_ENV: process.env.NODE_ENV,
-      COOKIE_DOMAIN: process.env.COOKIE_DOMAIN
-    }
+    'process.env.NODE_ENV': '"production"',
   }
 })
-
